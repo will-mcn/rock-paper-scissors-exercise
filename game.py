daa1331c5d336605f56c0ccf8967fdeb52c606a1
will-mcn <wam59@georgetown.edu>
2022-01-26 18:20:20 -0500

    User Input (In-class)

---
+++ game.py
@@ -0,0 +1,24 @@
+# this is the "game.py" file
+print("Rock, Paper, Scissors, Shoot!")
+
+print("-------------------")
+
+# Ask for User Input 
+user_choice = input("rock, paper, or scissors? ")
+print(user_choice)
+
+# Validations
+
+
+
+
+# Computer Choice
+
+
+
+
+
+# Determine the Winner
+
+
+# Final Results
